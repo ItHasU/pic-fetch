1db5c8df6e78e1c26acfaa98a2d1150e07225c2c
Matthieu Aubry <ithasu@gmail.com>
2021-11-18 23:13:05 +0100

    Fetch a limited amount of images.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pic-fetch
 Fetch pictures from an inbox.
+
+## Howto use
+
+Create a file named `config.ini` in the project's folder, with the following format:
+```
+[IMAP]
+Server=imap.gmail.com  # URL of the imap server
+Username=xxx@gmail.com # Login for the imap server
+Password=xxx           # 
+MaxMessagesCount=20
+
+[PICTURES]
+Cache=cache
+MaxPicturesCount=30
+```
+
+Run the program with the following command:
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-import imaplib
+import configparser
 import email
+import getpass
+import imaplib
+import os
+import uuid
 from email.header import decode_header
-import webbrowser
-import os, getpass
 
-# account credentials
-username = "xxx@gmail.com"
-password = "xxx"
+SECTION_IMAP = "IMAP"
+PARAMS_IMAP_SERVER = "Server"
+PARAMS_IMAP_USER = "Username"
+PARAMS_IMAP_PASS = "Password"
+PARAMS_IMAP_MAX_MESSAGES = "MaxMessagesCount"
+
+SECTION_PICTURES = "PICTURES"
+PARAMS_PICTURES_FOLDER = "Cache"
+PARAMS_PICTURES_MAX = "MaxPicturesCount"
 
 
 def clean(text):
     # clean text for creating a folder
     return "".join(c if c.isalnum() else "_" for c in text)
 
-# number of top emails to fetch
-N = 3
+
+###############################################################################
+# Read configuration file #####################################################
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# account credentials
+server = config[SECTION_IMAP][PARAMS_IMAP_SERVER]
+username = config[SECTION_IMAP][PARAMS_IMAP_USER]
+password = config[SECTION_IMAP][PARAMS_IMAP_PASS]
+max_messages_count = int(config[SECTION_IMAP][PARAMS_IMAP_MAX_MESSAGES])
+pictures_folder = config[SECTION_PICTURES][PARAMS_PICTURES_FOLDER]
+pictures_max_count = int(config[SECTION_PICTURES][PARAMS_PICTURES_MAX])
+
+# Read emails #################################################################
 
 # create an IMAP4 class with SSL, use your email provider's IMAP server
-imap = imaplib.IMAP4_SSL("imap.gmail.com")
+imap = imaplib.IMAP4_SSL(server)
 # authenticate
 imap.login(username, password)
 
@@ -26,81 +47,88 @@ imap.login(username, password)
 status, messages = imap.select("INBOX")
 
 # total number of emails
-messages = int(messages[0])
+messages_count = int(messages[0])
+read_count = min(messages_count, max_messages_count)
 
-for i in range(messages, messages-N, -1):
+pictures = []
+
+for i in range(read_count):
+    if len(pictures) >= pictures_max_count:
+        break
+
+    print(
+        "Reading message %d/%d (out of %d message(s))"
+        % (i + 1, read_count, messages_count),
+        end=None,
+    )
+
     # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
+    res, msg = imap.fetch(str(messages_count - i), "(RFC822)")
     for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes email into a message object
-            msg = email.message_from_bytes(response[1])
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # if it's a bytes, decode to str
-                subject = subject.decode(encoding)
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                From = From.decode(encoding)
-            print("Subject:", subject)
-            print("From:", From)
-            # if the email message is multipart
-            if msg.is_multipart():
-                # iterate over email parts
-                for part in msg.walk():
-                    # extract content type of email
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
+        if len(pictures) >= pictures_max_count:
+            break
+        if not isinstance(response, tuple):
+            continue
 
-                    filename = part.get_filename()
-                    if filename is not None and (filename.endswith(".jpg") or filename.endswith(".jpeg")):
-                        open(filename, "wb").write(part.get_payload(decode=True))
-                        print(filename)
+        # parse a bytes email into a message object
+        msg = email.message_from_bytes(response[1])
 
-                    # if content_type == "text/plain" and "attachment" not in content_disposition:
-                    #     # print text/plain emails and skip attachments
-                    #     # print(body)
-                    #     pass
-                    # elif "attachment" in content_disposition:
-                    #     # download attachment
-                    #     filename = part.get_filename()
-                    #     print(filename)
-                    #     # if filename:
-                    #     #     folder_name = clean(subject)
-                    #     #     if not os.path.isdir(folder_name):
-                    #     #         # make a folder for this email (named after the subject)
-                    #     #         os.mkdir(folder_name)
-                    #     #     filepath = os.path.join(folder_name, filename)
-                    #     #     # download attachment and save it
-                    #     #     open(filepath, "wb").write(part.get_payload(decode=True))
-            # else:
-            #     # extract content type of email
-            #     content_type = msg.get_content_type()
-            #     # get the email body
-            #     body = msg.get_payload(decode=True).decode()
-            #     print(content_type)
-            #     if content_type == "text/plain":
-            #         # print only text email parts
-            #         # print(body)
-            #         pass
-            # if content_type == "text/html":
-            #     # if it's HTML, create a new HTML file and open it in browser
-            #     folder_name = clean(subject)
-            #     if not os.path.isdir(folder_name):
-            #         # make a folder for this email (named after the subject)
-            #         os.mkdir(folder_name)
-            #     filename = "index.html"
-            #     filepath = os.path.join(folder_name, filename)
-            #     # write the file
-            #     open(filepath, "w").write(body)
-            # print("="*100)
+        # if the email message is multipart
+        if not msg.is_multipart():
+            continue
+
+        # decode the email subject
+        id, encoding = decode_header(msg["Message-Id"])[0]
+        if isinstance(id, bytes):
+            # if it's a bytes, decode to str
+            id = id.decode(encoding)
+
+        # iterate over email parts
+        part_index = 0
+        for part in msg.walk():
+            if len(pictures) >= pictures_max_count:
+                break
+            # extract content type of email
+            content_type = part.get_content_type()
+            content_disposition = str(part.get("Content-Disposition"))
+            try:
+                # get the email body
+                body = part.get_payload(decode=True).decode()
+            except:
+                pass
+
+            filename = part.get_filename()
+            if filename is not None and (
+                filename.endswith(".jpg") or filename.endswith(".jpeg")
+            ):
+                filename = os.path.join(
+                    pictures_folder, clean(id) + str(part_index) + ".jpg"
+                )
+                pictures.append(filename)  # Store picture to keep it
+                part_index += 1
+                open(filename, "wb").write(part.get_payload(decode=True))
+
 # close the connection and logout
 imap.close()
 imap.logout()
+
+
+# Clean pictures
+from os import listdir
+from os.path import isfile, join
+
+files_to_delete = [
+    os.path.join(pictures_folder, f)
+    for f in listdir(pictures_folder)
+    if not f.startswith(".")
+]
+
+for f in pictures:
+    files_to_delete.remove(f)
+
+for f in files_to_delete:
+    print("Delete %s" % f)
+    try:
+        os.remove(f)
+    except:
+        pass
