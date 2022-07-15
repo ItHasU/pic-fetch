b26d9d67dcc6a9d5774447ef03f20a031f4d5867
Matthieu Aubry <ithasu@gmail.com>
2022-07-15 13:21:03 +0000

    Format.

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def clean(text):
     # clean text for creating a folder
     return "".join(c if c.isalnum() else "_" for c in text)
 
+
 ###############################################################################
 # Actions - Whitelist
 ###############################################################################
+
 
 def whitelist_add_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
     if not mm.is_sender_admin(message):
@@ -26,7 +28,12 @@ def whitelist_add_cb(mm: MailManager, message: email.message.EmailMessage) -> No
     print(f"Adding {email_to_add} to whitelist...")
     mm.whitelist_add(email_to_add)
     mm.reply(message, f"{email_to_add} added to whitelist")
-    mm.send(email_to_add, "You have been added to the whitelist", f"You can now send pictures to {mm._imap_username}")
+    mm.send(
+        email_to_add,
+        "You have been added to the whitelist",
+        f"You can now send pictures to {mm._imap_username}",
+    )
+
 
 def whitelist_rm_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
     if not mm.is_sender_admin(message):
@@ -37,10 +44,15 @@ def whitelist_rm_cb(mm: MailManager, message: email.message.EmailMessage) -> Non
     print(f"Removing {email_to_remove} from whitelist...")
     mm.whitelist_remove(email_to_remove)
     mm.reply(message, f"{email_to_remove} removed from whitelist")
-    mm.send(email_to_remove, "You have been removed from the whitelist", f"You can no longer send pictures to {mm._imap_username}")
+    mm.send(
+        email_to_remove,
+        "You have been removed from the whitelist",
+        f"You can no longer send pictures to {mm._imap_username}",
+    )
+
 
 def ping_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
-    sender = email['From']
+    sender = email["From"]
     if not mm.whitelist_has(sender):
         # not in whitelist, ignore mail, but do not fail
         # we don't want to reply to the sender or spam the admin
@@ -50,6 +62,7 @@ def ping_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
     print("PING from: " + sender)
     mm.reply(message, f"Pong")
 
+
 def default_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
     if not mm.whitelist_has(message["From"]):
         # not in whitelist, ignore mail, but do not fail
@@ -58,6 +71,7 @@ def default_cb(mm: MailManager, message: email.message.EmailMessage) -> None:
         return
     print("Default callback")
     mm.reply(message, "I don't know what to do with this message")
+
 
 ###############################################################################
 # Read configuration file #####################################################
